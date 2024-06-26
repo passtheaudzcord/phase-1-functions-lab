b133f9f9a7754f6cc1d94d7f5585b50a3c20c239
Audrey <chipmanaudrey@gmail.com>
2024-06-26 16:11:19 -0700

    function with rideshare app

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (pickupLocation) => {
+    //if(pickupLocation > 42) {
+        //return pickupLocation - 42;
+    //} else {
+        //return 42 - pickupLocation;
+    //}
+    return Math.abs(pickupLocation - 42);
+}
+
+const distanceFromHqInFeet = (pickupLocation) => {
+    return distanceFromHqInBlocks(pickupLocation) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+return (Math.abs(start-destination)) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let totalFeet = distanceTravelledInFeet(start, destination)
+    //first 400 ft free
+    if (totalFeet <= 400) {
+        return 0
+    } else if (totalFeet >= 400 && totalFeet <= 2000) {
+        return (totalFeet - 400) * .02;
+    } else if (totalFeet >= 2000 && totalFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+}
+
+
